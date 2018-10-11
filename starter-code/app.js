a540bce083d67ee9285b0b386fe208fc8db8c923
Evapm <eva.p.mejias@gmail.com>
2018-10-11 15:29:14 +0200

    layouts

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,19 +1,44 @@
 const express = require ('express');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+
 const app = express();
 const port = 3000;
 
-app.use(express.static('starter-code/public'));
+app.use(express.static('public'));
+
+app.use(expressLayouts);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main');
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    const isLogged = true;
+    const name = "Paquito";
+    const title = 'Mi título';
+    const num = 2;
+    const obj = {
+        prop: 'cosilla',
+    };
+    const arr = [1,2,3,4];
+    const objetoAPasar = {
+        title: title,
+        number: num,
+        obj: obj,
+        array: arr,
+        isLogged: isLogged,
+        name: name,
+    }
+
+    res.render('index', objetoAPasar);
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/views/about.html');
+    res.render('about')
 });
 
 app.get('/gallery', (req, res) => {
-    res.sendFile(__dirname + '/views/gallery.html');
+    res.render('gallery')
 });
 app.listen(port, (error) => {
     error ? console.log(error) : console.log('running on 3000');
